0f1fc681527fe9c818a2836289c089679226bf5b
Will Zhang <willyzhysh@gmail.com>
2020-06-13 22:45:39 +1200

    changed image slider to use imageslider library, removed imageSliderAdaptor

--- app/src/main/java/com/example/showtech/DetailsActivity.java
+++ app/src/main/java/com/example/showtech/DetailsActivity.java
@@ -1,22 +1,27 @@
 package com.example.showtech;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.showtech.utils.ImageSliderAdapter;
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 
 import java.util.Objects;
 
-public class DetailsActivity extends AppCompatActivity{
+public class DetailsActivity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener{
+
+
 
     static class ViewHolder {
-        ViewPager viewPager;
         TextView title, name, price, description;
+        SliderLayout imageSlider;
     }
 
     private Electronic item;
@@ -30,24 +35,45 @@ public class DetailsActivity extends AppCompatActivity{
 
         vh = new ViewHolder();
         vh.title = (TextView) findViewById(R.id.item_title);
-        vh.viewPager = (ViewPager) findViewById(R.id.item_images);
         vh.name = (TextView) findViewById(R.id.name);
         vh.price = (TextView) findViewById(R.id.price);
         vh.description = (TextView) findViewById(R.id.description);
+        vh.imageSlider = (SliderLayout) findViewById(R.id.slider);
 
         Intent intent = getIntent();
         item = (Electronic) intent.getSerializableExtra(ListActivity.EXTRA_ITEM);
         String title = item.getName();
         vh.title.setText(title);
 
-        ImageSliderAdapter adapter = new ImageSliderAdapter(this, item);
-        vh.viewPager.setAdapter(adapter);
+        for(int imageID : item.getImages()){
+            TextSliderView textSliderView = new TextSliderView(this);
+            textSliderView
+                    .description(title)
+                    .image(imageID)
+                    .setScaleType(BaseSliderView.ScaleType.CenterCrop)
+                    .setOnSliderClickListener(this);
+
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",title);
+
+            vh.imageSlider.addSlider(textSliderView);
+        }
+        vh.imageSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        vh.imageSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        vh.imageSlider.setCustomAnimation(new DescriptionAnimation());
+        vh.imageSlider.setDuration(4000);
 
         vh.name.setText(item.getName());
         String price = "$ " + item.getPrice().toString();
         vh.price.setText(price);
         vh.description.setText(item.getDescription());
 
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(this,slider.getBundle().get("extra") + "",Toast.LENGTH_SHORT).show();
     }
 
     public void back(View view) {
